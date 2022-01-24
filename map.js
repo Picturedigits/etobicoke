4be4175fe558c2375ab5f444fe26a9c7b9345009
ilyankou <ilyankou@gmail.com>
2022-01-24 18:51:06 +0000

    Basic map prototype in maplibre

---
+++ map.js
@@ -0,0 +1,156 @@
+var yearDropdown = document.getElementById('census-year-dropdown');
+var varDropdown = document.getElementById('census-variable-dropdown');
+
+// Generate a list of all census years from `metadata.js`
+Object.keys(metadata).forEach(function(year) {
+  yearDropdown[yearDropdown.options.length] = new Option(year, year)
+});
+
+const etobicokeBounds = new maplibregl.LngLatBounds(
+  [-79.650, 43.580],
+  [-79.465, 43.765]
+)
+
+// Initialize the map
+var map = new maplibregl.Map({
+  container: 'map-l',
+  style: 'https://api.maptiler.com/maps/pastel/style.json?key=fXc4knA6xdFWVhZVbmqa',
+  center: [-79.55, 43.67],
+  zoom: 10,
+});
+
+// Initialize the map
+var map2 = new maplibregl.Map({
+  container: 'map-r',
+  style: 'https://api.maptiler.com/maps/pastel/style.json?key=fXc4knA6xdFWVhZVbmqa',
+  center: [-79.55, 43.67],
+  zoom: 10,
+});
+
+syncMaps(map, map2)
+
+
+map.on('load', function () {
+
+  // Add individual geojson sources + layers for each census year
+  Object.keys(metadata).forEach(function(year) {
+    
+    // Add source
+    map.addSource(year, {
+      'type': 'geojson',
+      'data': './geojson/' + year + '.geojson'
+    });
+
+    // Add polygon layer
+    map.addLayer({
+      'id': year,
+      'type': 'fill',
+      'source': year,
+      'layout': {
+        'visibility': 'none'
+      },
+      'paint': {
+        'fill-color': 'gray',
+        'fill-opacity': 0.9,
+        'fill-outline-color': 'white'
+      }
+    })
+
+    // Add labels
+    map.addLayer({
+      'id': year + '-labels',
+      'type': 'symbol',
+      'source': year,
+      'minzoom': 9,
+      'layout': {
+        'visibility': 'none',
+        'text-field': '',
+        'text-size': 10,
+      },
+      'paint': {
+        'text-color': '#000000',
+        'text-halo-color': 'rgba(255,255,255,0.5)',
+        'text-halo-width': 2
+      }
+    });
+
+  })
+
+
+
+  // On census year change, reload polygons
+  yearDropdown.addEventListener('change', function() {
+
+    // Hide all polygons & layers
+    Object.keys(metadata).forEach(function(y) {
+      map.setLayoutProperty(y, 'visibility', 'none');
+      map.setLayoutProperty(y + '-labels', 'visibility', 'none');
+    });
+
+    var year = yearDropdown.value;
+
+    // Update list of variables for the particular census year
+    varDropdown.options.length = 0;
+    Object.keys(metadata[year]).forEach(function(v) {
+      varDropdown[varDropdown.options.length] = new Option(v, v)
+    });
+
+    // Show choropleth & labels for the year
+    map.setLayoutProperty(year, 'visibility', 'visible');
+    map.setLayoutProperty(year + '-labels', 'visibility', 'visible');
+
+    // By default, showing first variable
+    varDropdown.selectedIndex = 0;
+    varDropdown.dispatchEvent(new Event('change'));
+  });
+
+  // On variable change, update choropleth
+  varDropdown.addEventListener('change', function() {
+
+    var year = yearDropdown.value;
+    var variable = varDropdown.value;
+
+    // Update choropleth
+    map.setPaintProperty(
+      year,
+      'fill-color',
+      [
+        'interpolate',
+        ['linear'],
+        ['to-number', ['get', variable]],
+        parseFloat(metadata[year][variable]['min']),
+        ['to-color', '#f7fbff'],
+        parseFloat(metadata[year][variable]['max']),
+        ['to-color', '#08306b']
+      ]
+    )
+
+    // Update labels
+    map.setLayoutProperty(
+      year + '-labels',
+      'text-field',
+      [
+        'format',
+        ['get', 'ct'],
+        {
+          'text-font': ['literal', ['DIN Offc Pro Italic', 'Arial Unicode MS Regular']],
+          'font-scale': 0.9,
+        },
+        '\n',
+        {},
+        ['get', variable],
+        {
+          'font-scale': 1.1
+        }
+      ]
+    )
+
+  });
+
+  // Initialize map with 1951, first variable
+  yearDropdown.dispatchEvent(new Event('change'));
+
+  // Fit Etobicoke
+  map.fitBounds( etobicokeBounds, { padding: 10 } )
+
+});
