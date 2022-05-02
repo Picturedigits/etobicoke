59fd8e8a0e9affb163af39aee28a9b6a2394c3d3
ilyankou <ilyankou@gmail.com>
2022-05-02 13:42:45 +0100

    Add attendance areas overlay

---
+++ js/overlays.js
@@ -0,0 +1,7 @@
+const overlays = {
+  '1961': [{
+    name: '1962-63-attendance-areas',
+    displayName: '1962-63 Attendance Areas',
+    data: './overlays/etobicoke-1962-63-attendance-areas.geojson'
+  }]
+}
