68461fa2e228cefc8d21e7799c900ece573276a0
ilyankou <ilyankou@gmail.com>
2026-05-26 23:13:24 +0100

    Add schools overlay to all years; add closing date to school names

--- js/overlays.js
+++ js/overlays.js
@@ -1,4 +1,12 @@
 const overlays = {
+
+  '1951': [{
+    name: 'schools',
+    displayName: 'Schools',
+    type: 'point',
+    data: './overlays/schools.geojson'
+  }],
+
   '1961': [{
     name: '1962-63-attendance-areas',
     displayName: '1962-63 Attendance Areas',
@@ -8,5 +16,40 @@ const overlays = {
     displayName: 'Schools',
     type: 'point',
     data: './overlays/schools.geojson'
-  }]
+  }],
+
+  '1971': [{
+    name: 'schools',
+    displayName: 'Schools',
+    type: 'point',
+    data: './overlays/schools.geojson'
+  }],
+
+  '1981': [{
+    name: 'schools',
+    displayName: 'Schools',
+    type: 'point',
+    data: './overlays/schools.geojson'
+  }],
+
+  '1991': [{
+    name: 'schools',
+    displayName: 'Schools',
+    type: 'point',
+    data: './overlays/schools.geojson'
+  }],
+
+  '2001': [{
+    name: 'schools',
+    displayName: 'Schools',
+    type: 'point',
+    data: './overlays/schools.geojson'
+  }],
+
+  '2011': [{
+    name: 'schools',
+    displayName: 'Schools',
+    type: 'point',
+    data: './overlays/schools.geojson'
+  }],
 }
